cf994f0aa5adade873da3aa73bc7b67bf6e3cd1b
Alexei Sholik <alcosholik@gmail.com>
2013-03-10 14:21:09 +0200

    Add DateTime tests

---
+++ test/datetime_test.ex
@@ -0,0 +1,11 @@
+# Test interop between Date and Time.
+
+defmodule DateTimeTest do
+  use ExUnit.Case, async: true
+
+  test :epoch do
+    assert Date.to_sec(Date.epoch, 0) == Time.to_sec(Time.epoch)
+    assert Date.epoch(:sec) == Time.epoch(:sec)
+    assert Date.from(Time.epoch, :timestamp_since_year_0) == Date.epoch
+  end
+end
