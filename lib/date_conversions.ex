bb753a6b667829e0b7084f6b0a924ff80eb08966
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-02-21 22:08:04 -0600

    Reorganize Date.Conversions

--- lib/date_conversions.ex
+++ lib/date_conversions.ex
@@ -1,8 +1,8 @@
+defrecord Date.Gregorian, [:date, :time, :tz]
+
 defprotocol Date.Conversions do
   def to_gregorian(date)
 end
-
-defrecord Date.Gregorian, [:date, :time, :tz]
 
 defimpl Date.Conversions, for: Date.Gregorian do
   def to_gregorian(Date.Gregorian[date: date, time: time, tz: tz]) do
