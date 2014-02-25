9e1ac67611a37520f4e92f9fec9cb8730eb2eef3
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-02-25 00:03:24 -0600

    Handle all edge cases in DST detection code + tests

--- lib/timezone/timezone_dst.ex
+++ lib/timezone/timezone_dst.ex
@@ -6,7 +6,7 @@ defmodule Timezone.Dst do
   @doc """
   Check if the provided datetime is in daylight savings time
   """
-  @spec is_dst?(DateTime.t) :: true | false | :ambiguous_time | :invalid_time
+  @spec is_dst?(DateTime.t) :: true | false | :ambiguous_time | :doesnt_exist
   def is_dst?(DateTime[timezone: TimezoneInfo[dst_start_day: :undef]]), do: false
   def is_dst?(DateTime[year: year, month: month, day: day, hour: hour, minute: min, second: sec, timezone: tz]) do
     TimezoneInfo[
@@ -24,24 +24,64 @@ defmodule Timezone.Dst do
       :equal_to_end ->
         is_dst_end_time(time_to_minutes({hour, min, sec}), time_to_minutes(dst_end_time), dst_shift)
       result ->
-        result
+        cond do
+          # If DST ends the next day at 00:00, then after 23:00 the day before, we're ambiguous
+          dst_end_day - 1 === current_day and time_to_minutes(dst_end_time) == 0 ->
+            cond do
+              time_to_minutes({hour, min, sec}) >= (23 * 60) -> :ambiguous_time
+              true -> result
+            end
+          # Likewise, if DST started the night before at midnight (24:00), then before 01:00 of the current day, the time doesn't exist
+          dst_start_day + 1 === current_day and time_to_minutes(dst_start_time) == (24 * 60) ->
+            cond do
+              time_to_minutes({hour, min, sec}) < 60 -> :doesnt_exist
+              true -> result
+            end
+          true ->
+            result
+        end
     end
    end
 
   defp is_dst_start_time(current_time, start_time, _shift) when current_time < start_time, do: false
-  defp is_dst_start_time(current_time, start_time, shift) when current_time >= (start_time + shift), do: true
-  defp is_dst_start_time(_, _, _), do: :invalid_time
+  defp is_dst_start_time(current_time, start_time, shift) do
+      case (start_time + shift) do
+        # When start time is late, say 2400, it rolls over in to the next day with the shift
+        shifted when shifted < start_time and current_time < start_time ->
+          (current_time + shift) >= shifted
+        # When it doesn't roll over, normal checks apply
+        shifted when shifted > start_time and current_time >= shifted ->
+          true
+        shifted when shifted > start_time and current_time < shifted and current_time >= start_time ->
+          :doesnt_exist
+        _ ->
+          :doesnt_exist
+      end
+  end
 
-  defp is_dst_end_time(current_time, end_time, _shift) when current_time < end_time, do: true
-  defp is_dst_end_time(current_time, end_time, shift) when current_time >= (end_time + shift), do: false
-  defp is_dst_end_time(_current_time, _end_time, _shift), do: :ambiguous_time
+  defp is_dst_end_time(current_time, end_time, shift) do
+    # Ambigous for the hour before it ends
+    case (end_time - shift) do
+      # When the end is at 0:00 and the current is >= 0:00, we're out of DST
+      shifted when shifted < 0 and current_time >= 0 -> false
+      # When the end is at any other point in the day, and the current time is between the shifted and end time, ambigous
+      shifted when current_time >= shifted and current_time < end_time ->
+        :ambiguous_time
+      # When the end is at any other point in the day, and we're less than the shifted time, we're in dst
+      shifted when current_time < shifted ->
+        true
+      # When the end is at any other point in the day, and we're greater than the end_time, we're out of dst
+      _ ->
+        false
+    end
+  end
 
-  defp is_dst_date(start_day, _, current_day) when (current_day == start_day), do: :equal_to_start
-  defp is_dst_date(_, end_day, current_day) when (current_day == end_day),     do: :equal_to_end
-  defp is_dst_date(start_day, end_day, current_day) when (start_day < end_day) and ((current_day > start_day) and (current_day < end_day)), do: true
-  defp is_dst_date(start_day, end_day, current_day) when (start_day < end_day) and ((current_day < start_day) or (current_day > end_day)),  do: false
-  defp is_dst_date(start_day, end_day, current_day) when (start_day > end_day) and ((current_day < start_day) and (current_day > end_day)), do: false
-  defp is_dst_date(start_day, end_day, current_day) when (start_day > end_day) and ((current_day > start_day) or (current_day < end_day)),  do: true
+  defp is_dst_date(start_day, _, current_day) when current_day == start_day, do: :equal_to_start
+  defp is_dst_date(_, end_day, current_day)   when current_day == end_day,   do: :equal_to_end
+  defp is_dst_date(start_day, end_day, current_day) when start_day < end_day and (current_day > start_day and current_day < end_day), do: true
+  defp is_dst_date(start_day, end_day, current_day) when start_day < end_day and (current_day < start_day or current_day > end_day),  do: false
+  defp is_dst_date(start_day, end_day, current_day) when start_day > end_day and (current_day < start_day and current_day > end_day), do: false
+  defp is_dst_date(start_day, end_day, current_day) when start_day > end_day and (current_day > start_day or current_day < end_day),  do: true
 
   defp get_dst_day_of_year({weekday, day, month}, year) when (weekday == :last) or (weekday == 5) do
     month_num = Date.month_to_num(month)
