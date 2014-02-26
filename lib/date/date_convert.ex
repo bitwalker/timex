598ba5a16ffd105cdfacfc1c820695b5b085db46
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-02-25 23:52:29 -0600

    Use the correct abbreviation when presenting dates, based on DST status

--- lib/date/date_convert.ex
+++ lib/date/date_convert.ex
@@ -4,8 +4,17 @@ defprotocol Date.Convert do
 end
 
 defimpl Date.Convert, for: DateTime do
-  def to_gregorian(DateTime[year: y, month: m, day: d, hour: h, minute: min, second: sec, timezone: TimezoneInfo[standard_abbreviation: abbrev, gmt_offset_std: offset]]) do
-    { {y, m, d}, {h, min, sec}, {offset / 60, abbrev} }
+  def to_gregorian(
+    DateTime[
+      year: y, month: m, day: d, hour: h, minute: min, second: sec,
+      timezone: TimezoneInfo[standard_abbreviation: abbrev, dst_abbreviation: dst_abbrev, gmt_offset_std: std, gmt_offset_dst: dst]
+    ] = date) do
+    # Use the correct abbreviation depending on whether we're in DST or not
+    if Timezone.Dst.is_dst?(date) do
+      { {y, m, d}, {h, min, sec}, {(std + dst) / 60, dst_abbrev} }
+    else
+      { {y, m, d}, {h, min, sec}, {std / 60, abbrev} }
+    end
   end
   def to_erlang_datetime(DateTime[year: y, month: m, day: d, hour: h, minute: min, second: sec]) do
     { {y, m, d}, {h, min, sec} }
