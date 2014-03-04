07c8f633b5bc7c7091470a7a1ac298e8bc2ec559
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-03-04 17:05:00 -0600

    Add expm package definition

---
+++ package.exs
@@ -0,0 +1,10 @@
+Expm.Package.new(
+  name: "timex",
+  description: "Date/Time library for Elixir",
+  version: "0.4.3",
+  keywords: ["date", "time", "datetime", "timezone", "timer"],
+  maintainers: [
+    [name: "Paul Schoenfelder", email: "paulschoenfelder@gmail.com"]
+  ],
+  repositories: [[github: "bitwalker/timex"]]
+)
