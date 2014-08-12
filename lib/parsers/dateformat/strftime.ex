1e6992cad502d4a6c72389775df310c53a973999
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-08-12 09:36:41 -0500

    Add strftime parser

--- lib/parsers/dateformat/strftime.ex
+++ lib/parsers/dateformat/strftime.ex
@@ -1,2 +1,19 @@
-defmodule Timex.Parsers.DateFormat.Strftime do
+defmodule Timex.Parsers.DateFormat.StrftimeParser do
+  @moduledoc """
+  This module is responsible for parsing date strings using
+  the strftime formatting syntax.
+
+  See `Timex.DateFormat.Formatters.StrftimeFormatter` for more info.
+  """
+  use Timex.Parsers.DateFormat.Parser
+
+  @doc """
+  The tokenizer used by this parser.
+  """
+  defdelegate tokenize(format_string), to: Timex.Parsers.DateFormat.Tokenizers.Strftime
+
+  @doc """
+  Extracts the value for a given directive.
+  """
+  defdelegate parse_directive(date_string, directive), to: Timex.Parsers.DateFormat.DefaultParser
 end
