14157d6a76c2db0c58f47ebfcdba50263133ccfc
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-10-20 15:58:03 -0500

    Version 0.13.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Timex.Mixfile do
 
   def project do
     [ app: :timex,
-      version: "0.12.9",
+      version: "0.13.0",
       elixir: "~> 1.0.0",
       description: "A date/time library for Elixir",
       package: package,
