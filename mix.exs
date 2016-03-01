27ab61fad5ede44cf116fa7a2cb2ee9a586a9b33
Paul Schoenfelder <paulschoenfelder@gmail.com>
2016-02-29 18:05:21 -0600

    Version 1.0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Timex.Mixfile do
 
   def project do
     [ app: :timex,
-      version: "1.0.1",
+      version: "1.0.2",
       elixir: "~> 1.0",
       description: description,
       package: package,
