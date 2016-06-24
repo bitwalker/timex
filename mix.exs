7c736b6624b692b613a8ce2ec1086439780657e0
Paul Schoenfelder <paulschoenfelder@gmail.com>
2016-06-24 01:40:19 -0500

    Version 2.2.1 - fixes for Elixir 1.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Timex.Mixfile do
 
   def project do
     [ app: :timex,
-      version: "2.2.0",
+      version: "2.2.1",
       elixir: "~> 1.1",
       description: description(),
       package: package(),
