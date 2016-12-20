3d17f43244b3e59a9be4b72885e92cbee93e50cb
Paul Schoenfelder <paulschoenfelder@gmail.com>
2016-12-20 16:37:24 -0600

    Ensure tzdata is updated before running tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,5 @@
+# Ensure tzdata is up to date
+{:ok, _} = Application.ensure_all_started(:tzdata)
+_ = Tzdata.ReleaseUpdater.poll_for_update()
 ExUnit.configure(exclude: [skip: true])
 ExUnit.start()
