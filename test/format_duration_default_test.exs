8e232e083ea6e3e4ffa3d9acd79ac0079dbccd04
Slava Kisel <slavakisel@users.noreply.github.com>
2017-01-15 23:02:55 +0300

    Fix formatting of negative durations (#265)

--- test/format_duration_default_test.exs
+++ test/format_duration_default_test.exs
@@ -1,4 +1,19 @@
 defmodule DurationFormatDefaultTest do
   use ExUnit.Case, async: true
-  doctest Timex.Format.Duration.Formatters.Default
+  use Timex
+
+  alias Timex.Format.Duration.Formatters
+
+  doctest Formatters.Default
+
+  defp format(megaseconds, seconds, microseconds) do
+    Duration.from_erl({megaseconds, seconds, microseconds})
+      |> Formatters.Default.format
+  end
+
+  test "format negative duration" do
+    assert "PT0.9S" = format(0, -2, 1_100_000)
+    assert "PT2.0001S" = format(0, -1, -1_000_100)
+    assert "P45Y6M5DT21H12M34.590264S" = format(-1435, -180354, -590264)
+  end
 end
