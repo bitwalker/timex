c4a33ce223f47b688c25cd56a969a435014a47f2
Tan Jay Jun <tan@jayjun.com>
2017-07-26 14:57:40 +0800

    Remove unused DateTime inspect

--- lib/datetime/inspect.ex
+++ lib/datetime/inspect.ex
@@ -14,10 +14,6 @@ end
 # Only provide Inspect for versions < 1.5.0-rc*
 if System.version |> Version.parse! |> Version.compare(Version.parse!("1.5.0-rc.1")) == :lt do
   defimpl Inspect, for: DateTime do
-    def instpect(datetime, %{:structs => false} = opts) do
-      Inspect.Algebra.to_doc(datetime, opts)
-    end
-
     def inspect(%DateTime{} = d, _opts) do
       "#<DateTime(#{DateTime.to_iso8601(d)} #{d.time_zone})>"
     end
