86a2c490228a95fe0dbbb82ab9b623430aeb29aa
Dorian Karter <dkarter@users.noreply.github.com>
2017-09-12 11:23:27 -0500

    Update combine v0.7 -> v0.10 (#367)
    Cleans up deprecation errors related to `filter_map`.

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Timex.Mixfile do
 
   def deps do
     [{:tzdata, "~> 0.1.8 or ~> 0.5"},
-     {:combine, "~> 0.7"},
+     {:combine, "~> 0.10"},
      {:gettext, "~> 0.10"},
      {:ex_doc, "~> 0.13", only: :dev},
      {:benchfella, "~> 0.3", only: :dev},
