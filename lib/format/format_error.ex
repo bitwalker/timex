0fc554ea00db4f530aac139b051197326848fe93
Paul Schoenfelder <paulschoenfelder@gmail.com>
2018-05-08 18:55:42 -0400

    Fix #398 - handle FormatError with non-string reasons

--- lib/format/format_error.ex
+++ lib/format/format_error.ex
@@ -2,11 +2,15 @@ defmodule Timex.Format.FormatError do
   @moduledoc """
   Used for errors encountered during string formatting.
   """
-  alias Timex.Format.FormatError
-
   defexception message: "Invalid format!"
 
+  def exception([message: message]) when is_binary(message) do
+    %__MODULE__{message: message}
+  end
   def exception([message: message]) do
-    %FormatError{message: message}
+    %__MODULE__{message: "Invalid format: #{inspect message}"}
+  end
+  def exception(err) do
+    %__MODULE__{message: "Invalid format: #{inspect err}"}
   end
 end
