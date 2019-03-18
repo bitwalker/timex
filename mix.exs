c08b8ead8ef1254e218f1d30c67c346e07aa1f3f
Pierre Fenoll <pierrefenoll@gmail.com>
2019-03-18 11:54:12 +0100

    upgrade dep on tzdata
    Trying to prevent crashes like:
    
    ```
    14:46:37.530 initial_call=:tls_connection.init/1 function=crash_report/4 module=proc_lib line=508 file=proc_lib.erl pid=<0.2797.0> [error] Process #PID<0.2797.0> terminating
    ** (ErlangError) Erlang error: {:bad_action_from_state_function, {:reply, :undefined, {:error, :timeout}}}
        (stdlib) gen_statem.erl:1290: :gen_statem.parse_actions_reply/7
        (stdlib) gen_statem.erl:1206: :gen_statem.loop_event_actions_list/10
        (ssl) tls_connection.erl:133: :tls_connection.init/1
        (stdlib) proc_lib.erl:249: :proc_lib.init_p_do_apply/3
    Initial Call: :tls_connection.init/1
    Ancestors: [:tls_connection_sup, :ssl_connection_sup, :ssl_sup, #PID<0.2387.0>]
    Message Queue Length: 0
    Messages: []
    Links: [#PID<0.2796.0>, #PID<0.2393.0>]
    Dictionary: [ssl_pem_cache: :ssl_pem_cache, ssl_manager: :ssl_manager]
    Trapping Exits: true
    Status: :running
    Heap Size: 10958
    Stack Size: 27
    Reductions: 333794
    Neighbours:
        #PID<0.2796.0>
            Initial Call: :tls_sender.init/1
            Current Call: :gen_statem.loop_receive/3
            Ancestors: [:tzdata_release_updater, #PID<0.2513.0>, #PID<0.2512.0>]
            Message Queue Length: 0
            Links: [#PID<0.2797.0>]
            Trapping Exits: false
            Status: :waiting
            Heap Size: 376
            Stack Size: 12
            Reductions: 592
            Current Stacktrace:
                (stdlib) gen_statem.erl:880: :gen_statem.loop_receive/3
                (stdlib) proc_lib.erl:249: :proc_lib.init_p_do_apply/3
    14:46:37.531 function=do_restart/3 module=supervisor line=716 file=supervisor.erl pid=<0.2393.0> [error] Child :undefined of Supervisor :tls_connection_sup terminated
    ** (exit) an exception was raised:
        ** (ErlangError) Erlang error: {:bad_action_from_state_function, {:reply, :undefined, {:error, :timeout}}}
            (stdlib) gen_statem.erl:1290: :gen_statem.parse_actions_reply/7
            (stdlib) gen_statem.erl:1206: :gen_statem.loop_event_actions_list/10
            (ssl) tls_connection.erl:133: :tls_connection.init/1
            (stdlib) proc_lib.erl:249: :proc_lib.init_p_do_apply/3
    Pid: #PID<0.2797.0>
    Start Call: :tls_connection.start_link/?
    Restart: :temporary
    Shutdown: 4000
    Type: :worker
    ```

--- mix.exs
+++ mix.exs
@@ -50,7 +50,7 @@ defmodule Timex.Mixfile do
   end
 
   def deps do
-    [{:tzdata, "~> 0.1.8 or ~> 0.5"},
+    [{:tzdata, "~> 0.1.8 or ~> 0.5 or ~> 1.0.0-rc.0"},
      {:combine, "~> 0.10"},
      {:gettext, "~> 0.10"},
      {:ex_doc, "~> 0.13", only: [:docs]},
