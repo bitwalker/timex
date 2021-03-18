9d19b6ad57d3863826c2b75b3983a799a57ac275
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2021-03-18 14:12:00 -0400

    fix: use 'yesterday'/'tomorrow' in relative formatter

--- test/format_test.exs
+++ test/format_test.exs
@@ -7,6 +7,8 @@ defmodule DateFormatTest.GeneralFormatting do
     utc_date = Timex.to_datetime({{2016, 2, 8}, {12, 0, 1}})
     cst_date = Timex.Timezone.convert(utc_date, "America/Chicago")
     assert Timex.from_now(utc_date, ref_date) == Timex.from_now(cst_date, ref_date)
+
+    assert Timex.from_now(Timex.shift(DateTime.utc_now(), days: -1), "en") == "yesterday"
   end
 
   test "converts maps and tuples before formatting" do
