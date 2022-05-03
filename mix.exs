43c998e34bce70826942711e47017055f6fbe0c4
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2022-05-03 18:20:32 -0400

    fix: update deps

--- mix.exs
+++ mix.exs
@@ -55,7 +55,7 @@ defmodule Timex.Mixfile do
 
   def deps do
     [
-      {:tzdata, "~> 1.0"},
+      {:tzdata, "~> 1.1"},
       {:combine, "~> 0.10"},
       {:gettext, "~> 0.10"},
       {:ex_doc, "~> 0.13", only: [:docs]},
