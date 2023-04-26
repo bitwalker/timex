elixir-lang/elixir

commit 5a583c753b96865a7cdec2fb4c1ab9c96b836d24

Consistently merge precisions in Calendar subsecond operations, closes #12303

lib/elixir/lib/calendar/datetime.ex
@@ -1493,6 +1493,13 @@ defmodule DateTime do
       iex> dt |> DateTime.add(1, :day, FakeTimeZoneDatabase)
       #DateTime<2019-04-01 02:00:00+02:00 CEST Europe/Copenhagen>
 
+  This operation merges the precision of the naive date time with the given unit:
+
+      iex> result = DateTime.add(~U[2014-10-02 00:29:10Z], 21, :millisecond)
+      ~U[2014-10-02 00:29:10.021Z]
+      iex> result.microsecond
+      {21000, 3}
+
   """
   @doc since: "1.8.0"
   @spec add(
@@ -1537,6 +1544,7 @@ defmodule DateTime do
 
     ppd = System.convert_time_unit(86400, :second, unit)
     total_offset = System.convert_time_unit(utc_offset + std_offset, :second, unit)
+    precision = max(Calendar.ISO.time_unit_to_precision(unit), precision)
 
     result =
       datetime

lib/elixir/lib/calendar/iso.ex
@@ -248,6 +248,17 @@ defmodule Calendar.ISO do
   defguardp is_std_offset(offset) when is_integer(offset)
 
   @doc """
+  Converts a `t:System.time_unit/0` to precision.
+
+  Integer-based time units always get maximum precision.
+  """
+  def time_unit_to_precision(:nanosecond), do: 6
+  def time_unit_to_precision(:microsecond), do: 6
+  def time_unit_to_precision(:millisecond), do: 3
+  def time_unit_to_precision(:second), do: 0
+  def time_unit_to_precision(_), do: 6
+
+  @doc """
   Parses a time `string` in the `:extended` format.
 
   For more information on supported strings, see how this

lib/elixir/lib/calendar/naive_datetime.ex
@@ -379,7 +379,7 @@ defmodule NaiveDateTime do
   It can also work with subsecond precisions:
 
       iex> NaiveDateTime.add(~N[2014-10-02 00:29:10], 2_000, :millisecond)
-      ~N[2014-10-02 00:29:12]
+      ~N[2014-10-02 00:29:12.000]
 
   As well as days/hours/minutes:
 
@@ -390,16 +390,12 @@ defmodule NaiveDateTime do
       iex> NaiveDateTime.add(~N[2015-02-28 00:29:10], 60, :minute)
       ~N[2015-02-28 01:29:10]
 
-  This operation keeps the precision of the naive date time:
+  This operation merges the precision of the naive date time with the given unit:
 
-      iex> NaiveDateTime.add(~N[2014-10-02 00:29:10.021], 21, :second)
-      ~N[2014-10-02 00:29:31.021]
-
-  And ignores any changes below the precision:
-
-      iex> hidden = NaiveDateTime.add(~N[2014-10-02 00:29:10], 21, :millisecond)
-      iex> hidden.microsecond # ~N[2014-10-02 00:29:10]
-      {21000, 0}
+      iex> result = NaiveDateTime.add(~N[2014-10-02 00:29:10], 21, :millisecond)
+      ~N[2014-10-02 00:29:10.021]
+      iex> result.microsecond
+      {21000, 3}
 
   Operations on top of gregorian seconds or the Unix epoch are optimized:
 
@@ -440,6 +436,7 @@ defmodule NaiveDateTime do
       )
       when is_integer(amount_to_add) do
     ppd = System.convert_time_unit(86400, :second, unit)
+    precision = max(Calendar.ISO.time_unit_to_precision(unit), precision)
 
     naive_datetime
     |> to_iso_days()

lib/elixir/lib/calendar/time.ex
@@ -493,6 +493,13 @@ defmodule Time do
       iex> Time.add(~T[17:10:05], 30, :minute)
       ~T[17:40:05]
 
+  This operation merges the precision of the time with the given unit:
+
+      iex> result = Time.add(~T[00:29:10], 21, :millisecond)
+      ~T[00:29:10.021]
+      iex> result.microsecond
+      {21000, 3}
+
   """
   @doc since: "1.6.0"
   @spec add(Calendar.time(), integer, :hour | :minute | System.time_unit()) :: t
@@ -511,6 +518,7 @@ defmodule Time do
     amount_to_add = System.convert_time_unit(amount_to_add, unit, :microsecond)
     total = time_to_microseconds(time) + amount_to_add
     parts = Integer.mod(total, @parts_per_day)
+    precision = max(Calendar.ISO.time_unit_to_precision(unit), precision)
 
     {hour, minute, second, {microsecond, _}} =
       calendar.time_from_day_fraction({parts, @parts_per_day})
@@ -736,14 +744,14 @@ defmodule Time do
           hour: hour1,
           minute: minute1,
           second: second1,
-          microsecond: {microsecond1, @parts_per_day}
+          microsecond: {microsecond1, _}
         },
         %{
           calendar: Calendar.ISO,
           hour: hour2,
           minute: minute2,
           second: second2,
-          microsecond: {microsecond2, @parts_per_day}
+          microsecond: {microsecond2, _}
         },
         unit
       ) do

bitwalker/timex

commit 4c00c0bb681619ff7953db3a73e88decd107de1b

Fix: preserve datetime precision after Timex.shift/2
Resolves https://github.com/bitwalker/timex/issues/731

Unfortunately this broke with https://github.com/elixir-lang/elixir/commit/5a583c753b96865a7cdec2fb4c1ab9c96b836d24
which was release with Elixir 1.14.3

Elixir 1.13.4:

```
iex(3)> DateTime.utc_now() |> IO.inspect() |> DateTime.truncate(:second) |> IO.inspect() |> Timex.shift(minutes: 1) |> IO.inspect()
~U[2023-04-13 09:56:10.136274Z]
~U[2023-04-13 09:56:10Z]
~U[2023-04-13 09:57:10Z]
```

Elixir 1.14.4:

```
iex(1)> DateTime.utc_now() |> IO.inspect() |> DateTime.truncate(:second) |> IO.inspect() |> Timex.shift(minutes: 1) |> IO.inspect()
~U[2023-04-13 09:55:16.405357Z]
~U[2023-04-13 09:55:16Z]
~U[2023-04-13 09:56:16.000000Z]
```

lib/datetime/datetime.ex
@@ -447,13 +447,17 @@ defimpl Timex.Protocol, for: DateTime do
           err
 
         %DateTime{} = datetime when shift != 0 ->
-          DateTime.add(datetime, shift, :microsecond, Timex.Timezone.Database)
+          datetime
+          |> DateTime.add(shift, :microsecond, Timex.Timezone.Database)
+          |> retain_precision(datetime)
 
         %DateTime{} = datetime ->
           datetime
 
-        {{ty, _, _}, %DateTime{} = orig} when ty in [:gap, :ambiguous] and shift != 0 ->
-          DateTime.add(orig, shift, :microsecond, Timex.Timezone.Database)
+        {{ty, _, _}, %DateTime{} = original} when ty in [:gap, :ambiguous] and shift != 0 ->
+          original
+          |> DateTime.add(shift, :microsecond, Timex.Timezone.Database)
+          |> retain_precision(datetime)
 
         {{ty, _a, _b} = amb, _} when ty in [:gap, :ambiguous] ->
           amb
@@ -478,6 +482,13 @@ defimpl Timex.Protocol, for: DateTime do
   catch
     :throw, {:error, _} = err ->
       err
+  end
+
+  defp retain_precision(
+         %DateTime{microsecond: {ms, _precision}} = new_datetime,
+         %DateTime{microsecond: {_ms, precision}} = _original_datetime
+       ) do
+    %{new_datetime | microsecond: {ms, precision}}
   end
 
   defp logical_shift(datetime, []), do: datetime

test/shift_test.exs
@@ -229,4 +229,44 @@ defmodule ShiftTests do
     expected = ~D[2017-12-31] |> Timex.to_datetime()
     assert expected === date
   end
+
+  describe "DateTime does not change precision" do
+    test "seconds" do
+      datetime = Timex.shift(~U[2023-04-13 08:00:00Z], minutes: 1)
+      expected = ~U[2023-04-13 08:01:00Z]
+      assert expected === datetime
+    end
+
+    test "milliseconds" do
+      datetime = Timex.shift(~U[2023-04-13 08:00:00.000Z], minutes: 1)
+      expected = ~U[2023-04-13 08:01:00.000Z]
+      assert expected === datetime
+    end
+
+    test "microseconds" do
+      datetime = Timex.shift(~U[2023-04-13 08:00:00.000000Z], minutes: 1)
+      expected = ~U[2023-04-13 08:01:00.000000Z]
+      assert expected === datetime
+    end
+  end
+
+  describe "NaiveDateTime does not change precision" do
+    test "seconds" do
+      datetime = Timex.shift(~N[2023-04-13 08:00:00Z], minutes: 1)
+      expected = ~N[2023-04-13 08:01:00Z]
+      assert expected === datetime
+    end
+
+    test "milliseconds" do
+      datetime = Timex.shift(~N[2023-04-13 08:00:00.000Z], minutes: 1)
+      expected = ~N[2023-04-13 08:01:00.000Z]
+      assert expected === datetime
+    end
+
+    test "microseconds" do
+      datetime = Timex.shift(~N[2023-04-13 08:00:00.000000Z], minutes: 1)
+      expected = ~N[2023-04-13 08:01:00.000000Z]
+      assert expected === datetime
+    end
+  end
 end